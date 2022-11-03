7e2918c94da861596b7532f7bb55e109381a9d0b
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-11-03 18:57:15 +0100

    Save User

--- src/main/java/dev/frizio/demo/resources/UserResource.java
+++ src/main/java/dev/frizio/demo/resources/UserResource.java
@@ -2,10 +2,15 @@ package dev.frizio.demo.resources;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import dev.frizio.demo.domains.User;
 import dev.frizio.demo.repos.UserMongoRepository;
@@ -21,14 +26,29 @@ public class UserResource {
 
     @GetMapping
     public Mono<ResponseEntity<List<User>>> findAll() {
-        return userMongoRepository.findAll()
+        return userMongoRepository
+                .findAll()
                 .collectList()
-                .map(users -> {
-                    if(users.isEmpty()) {
+                .map( users -> {
+                    if (users.isEmpty()) {
                         return ResponseEntity.noContent().build();
                     }
                     return ResponseEntity.ok(users);
-                });
+                  });
+    }
+
+    @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+    public Mono<ResponseEntity<User>> save(/*@Valid*/ @RequestBody User user) {
+        return userMongoRepository
+                .save(user)
+                .map( userSaved -> 
+                  UriComponentsBuilder.fromPath(("/{id}"))
+                    .buildAndExpand(userSaved.getId())
+                    .toUri() 
+                  )
+                .map( uri -> 
+                  ResponseEntity.created(uri).build() 
+                );
     }
 
 }
