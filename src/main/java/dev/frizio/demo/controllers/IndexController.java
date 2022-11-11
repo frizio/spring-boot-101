ad112caa53bb93cfc3527e59614e1d645275475c
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-11-11 10:33:45 +0100

    Thymeleaf initial page

--- src/main/java/dev/frizio/demo/controllers/IndexController.java
+++ src/main/java/dev/frizio/demo/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package dev.frizio.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
   
   @RequestMapping(value = {"", "/", "index"})
-  public String index() {
+  public String index(Model model) {
+    model.addAttribute("username", "frizio");
     return "index.html";
   }
 
