fc3ccc1e54faa87ddb15ebee77fdbc783a6bcf1a
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-11-11 10:01:16 +0100

    Mapping multiple path

--- src/main/java/dev/frizio/demo/controllers/IndexController.java
+++ src/main/java/dev/frizio/demo/controllers/IndexController.java
@@ -1,10 +1,12 @@
 package dev.frizio.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
   
+  @RequestMapping(value = {"", "/", "index"})
   public String index() {
     return "index.html";
   }
