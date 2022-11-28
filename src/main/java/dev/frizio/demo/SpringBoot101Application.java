003c27fed762b170dd8b7033b1ba68b22fc159fd
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-11-28 13:21:35 +0100

    Log runtime memory info

--- src/main/java/dev/frizio/demo/SpringBoot101Application.java
+++ src/main/java/dev/frizio/demo/SpringBoot101Application.java
@@ -1,8 +1,12 @@
 package dev.frizio.demo;
 
+import java.text.NumberFormat;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringBoot101Application {
 
@@ -10,7 +14,23 @@ public class SpringBoot101Application {
    * @param args
    */
   public static void main(String[] args) {
+
     SpringApplication.run(SpringBoot101Application.class, args);
+
+    Runtime runtime = Runtime.getRuntime();
+    final NumberFormat format = NumberFormat.getInstance();
+    final long maxMemory = runtime.maxMemory();
+    final long allocatedMemory = runtime.totalMemory();
+    final long freeMemory = runtime.freeMemory();
+    final long mb = 1024 * 1024;
+    final String mega = " MB";
+    log.info("========================== Memory Info ==========================");
+    log.info("Free memory: " + format.format(freeMemory / mb) + mega);
+    log.info("Allocated memory: " + format.format(allocatedMemory / mb) + mega);
+    log.info("Max memory: " + format.format(maxMemory / mb) + mega);
+    log.info("Total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / mb) + mega);
+    log.info("=================================================================\n");
+
   }
 
 }
