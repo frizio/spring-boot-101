634c7089124b227590e83e632a3769b50ac5587e
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-11-29 18:35:43 +0100

    Read properties with @Value

--- src/main/java/dev/frizio/demo/controllers/IndexController.java
+++ src/main/java/dev/frizio/demo/controllers/IndexController.java
@@ -1,13 +1,30 @@
 package dev.frizio.demo.controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
-  
+
+  @Autowired
+  private Environment environment;
+
+  @Value("${demo.message}")
+  private String message;
+
+  @Value("${demo.version}")
+  private int version;
+
   @RequestMapping(value = {"", "/", "index"})
   public String index() {
+    log.info("Property 1: " + message);
+    log.info("Property 2: " + version);
+    log.info("Property 3 (from env): " + environment.getProperty("JAVA_HOME"));
     return "index.html";
   }
 
