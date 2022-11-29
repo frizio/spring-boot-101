fbbbedc8d9d353c739174676b3f9fe2e9a26a37d
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-11-29 19:04:33 +0100

    Create PropertiesController

---
+++ src/main/java/dev/frizio/demo/controllers/PropertiesController.java
@@ -0,0 +1,34 @@
+package dev.frizio.demo.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/props")
+public class PropertiesController {
+
+    @Value("${demo.message}")
+    private String message;
+
+    @Value("${demo.version}")
+    private int version;
+
+    @Autowired
+    private Environment environment;
+
+
+    @GetMapping(value = "/value")
+    public String logPropsWithValue() {
+        log.info("Property 1: " + message);
+        log.info("Property 2: " + version);
+        log.info("Property 3 (from env): " + environment.getProperty("JAVA_HOME"));
+        return "OK";
+    }
+
+}
